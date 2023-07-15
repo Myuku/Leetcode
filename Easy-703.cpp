aa6e73f550f4cfb401e15ca711684466c4cd05ec
Myuku <junelee2002jh@gmail.com>
2023-07-14 22:29:06 -0700

    Finished Easy-703

---
+++ Easy-703.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int pos;
+
+    KthLargest(int k, vector<int>& nums) {
+        pos = k;
+        for (auto i : nums) {
+            pq.push(i);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+    }   
+    
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > pos) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
